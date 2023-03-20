41b1d95abb4977d0283e1f7ebee884ca7114c4c3
miksi1000 <mikkelfuglsang20@gmail.com>
2023-03-20 16:34:45 +0100

    Initital commit

---
+++ src/components/Cars.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="grid">
+      <div class="p-col-12 p-md-6 p-lg-3" v-for="car in cars" :key="car.id" style="margin-left: 10px">
+        
+        <!-- <h3>{{ car.name }}</h3> -->
+        <Car
+          @remove-car="$emit('remove-car', car.id)"
+          @add-favorite="$emit('add-favorite', car.id)"
+          :car="car"
+        />
+      </div>
+    </div>
+  </template>
+
+<script>
+import Car from "./Car"
+export default {
+    props: {
+        cars: Array
+    },
+    components: { Car },
+    emits: ["remove-car", "add-favorite"]
+};
+</script>
+
+<style>
+
+</style>
