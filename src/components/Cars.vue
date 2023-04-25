9465b95933331e54503962ba7ebf1aa4b81dad4d
miksi1000 <mikkelfuglsang20@gmail.com>
2023-04-25 11:57:25 +0200

    FIXED ALOT OF STUFF

--- src/components/Cars.vue
+++ src/components/Cars.vue
@@ -1,7 +1,6 @@
 <template>
-   <div><DropDown v-model="selectedcity" :options="cities" optionLabel="name" optionValue="code" placeholder="Vælg Afdeling" /> </div>
-        <p></p>
-        <div v-if="selectedcity === kolding" v-for="car in cars"></div>
+
+
     <div class="grid">
       <div class="p-col-12 p-md-6 p-lg-3" v-for="car in cars" :key="car.serialnumber" style="margin-left: 10px">
         
@@ -19,13 +18,9 @@ import Car from "./Car"
 export default {
   data(){
     return{
-      checked: true,
-      selectedcity: null,
-        cities: [
-			{name: 'Hillerød', code: '3400'},
-			{name: 'Odense', code: '5000'},
-            {name: 'Kolding', code: '6000'},
-    ],
+
+      
+    
     }
   },
     props: {
